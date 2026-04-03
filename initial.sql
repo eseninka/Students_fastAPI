9878f5f5566051a9ecd6d382ba3282033faa4d1e
eseninka <eseninka2706@gmail.com>
2026-04-03 23:06:11 +0300

    add initial for sql

---
+++ initial.sql
@@ -0,0 +1,11 @@
+create table students(
+student_id serial primary key,
+full_name varchar(255) not null UNIQUE
+);
+
+create table data_about_students
+(record_id serial primary key,
+estimation smallint not null CHECK (estimation >= 2 AND estimation <= 5),
+student_id int not null,
+constraint key_users foreign key(student_id) references students(student_id)
+)
